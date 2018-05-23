997d18500139017d0a331993304d7024ad403d62
rodrigo4244 <rodrigo.4244@hotmail.com>
2018-05-23 11:42:58 -0300

    fix: default PORT number

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@
 
 export const Config = {
   NODE_ENV: process.env.NODE_ENV,
-  PORT: process.env.PORT || 3010,
+  PORT: process.env.PORT || 3006,
   HEIMDALL_URL: process.env.HEIMDALL_URL,
   DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING,
   DB_TEST_CONNECTION_STRING: process.env.DB_TEST_CONNECTION_STRING,
