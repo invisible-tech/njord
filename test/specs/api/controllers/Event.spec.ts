debf7250f03af9397a018e070b65ea5870366204
rodrigo4244 <rodrigo.4244@hotmail.com>
2018-05-23 11:02:23 -0300

    test: add failing authentication test

--- test/specs/api/controllers/Event.spec.ts
+++ test/specs/api/controllers/Event.spec.ts
@@ -1,5 +1,6 @@
 'use strict'
 
+import * as assert from 'assert'
 import * as express from 'express'
 import 'mocha'
 import * as sinon from 'sinon'
@@ -13,18 +14,32 @@ const request = require('supertest')(app)
 const sandbox = sinon.sandbox.create()
 
 describe('createEvent', () => {
+  beforeEach(() => {
+    sandbox.stub(events, 'create').callsFake(async () => ({ happy: 'people' }))
+  })
+
   afterEach(() => {
     sandbox.restore()
   })
 
   it('should work', () => {
-    sandbox.stub(events, 'create').callsFake(async () => ({ happy: 'people' }))
-
     request
       .post('/api/v1/event')
       .set({ authentication: `Bearer ${process.env.NJORD_API_TOKEN}` })
       .send({ name: 'newInstance', metadata: {} })
       .expect(200)
+      .end((err: Error, res: Response) => {
+        assert.deepStrictEqual(res.body, {})
+        if (err) throw err
+      })
+  })
+
+  it('should fail authentication', () => {
+    request
+      .post('/api/v1/event')
+      .set({ authentication: 'Bearer invalidToken' })
+      .send({ name: 'newInstance', metadata: {} })
+      .expect(401)
       .end((err: Error, res: Response) => {
         if (err) throw err
       })
