58f3c4c50f4e089cad011c43d9c091063e59489f
rodrigo4244 <rodrigo.4244@hotmail.com>
2018-05-23 10:58:34 -0300

    refact: renamed IEvent to be EventInterface

--- src/models/events/index.ts
+++ src/models/events/index.ts
@@ -4,9 +4,9 @@ import * as mongodb from '@invisible/mongoose'
 import { Model } from 'mongoose'
 import * as softDelete from 'mongoose-delete'
 
-import { IEvent } from './interface'
+import { EventInterface } from './interface'
 import { schema } from './schema'
 
 schema.plugin(softDelete, { overrideMethods: 'all' })
 
-export const events: Model<IEvent> = mongodb.upsertModel({ name: 'events', schema })
+export const events: Model<EventInterface> = mongodb.upsertModel({ name: 'events', schema })
