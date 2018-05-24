16e6a9d53917263fd9d2332d8c03d2b7f439d801
Leonardo Kewitz <leokewitz@gmail.com>
2018-05-24 18:04:10 -0300

    refact: Remove unused variables and log successful events

--- src/api/controllers/Event.ts
+++ src/api/controllers/Event.ts
@@ -1,24 +1,18 @@
-'use strict'
-
 import * as logger from '@invisible/logger'
 import { NextFunction, Request, Response } from 'express'
-import { OK } from 'http-status'
-
 import { events } from '../../models/events'
 import { InternalServerError } from '../errors'
 
 const createEvent = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     const { name, metadata } = req.body
-    await events.create({
-      name,
-      timestamp: Date.now(),
-      metadata,
-    })
+    const doc = { name, metadata, timestamp: Date.now() }
+    await events.create(doc)
     res.json({})
+    logger.debug('Event created', doc)
   } catch (err) {
-    logger.error(err)
     next(new InternalServerError('Failed while creating the event on the database'))
+    logger.error(err)
   }
 }
 
