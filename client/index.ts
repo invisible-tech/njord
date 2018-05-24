7203c168242e93d2b5d8a7d922727f595d701788
Leonardo Kewitz <leokewitz@gmail.com>
2018-05-24 17:17:34 -0300

    style: Remove "use strict" and space
    You do not need use strict in TypeScript.
    Declare constants together

--- client/index.ts
+++ client/index.ts
@@ -1,13 +1,9 @@
-'use strict'
-
-import * as logger from '@invisible/logger'
 import * as env from 'env-var'
 import * as got from 'got'
 import * as url from 'url'
 
 const NJORD_HOSTNAME: string = env.get('NJORD_HOSTNAME').required().asString()
 const NJORD_API_TOKEN: string = env.get('NJORD_API_TOKEN').required().asString()
-
 const NJORD_ENDPOINT = url.resolve(NJORD_HOSTNAME, '/api/v1/event')
 
 const recordEvent = ({ name, metadata }: { name: string, metadata: object }): Promise<object> =>
